b5dc64d943733fbbfde5aea054caac52c010b5bb
courthive <charles@courthive.com>
2026-03-17 09:16:05 -0400

    feat: :children_crossing: export mocksEngine primitives to enhance end-user testing frameworks

--- src/assemblies/governors/mocksGovernor/mutate.ts
+++ src/assemblies/governors/mocksGovernor/mutate.ts
@@ -1,2 +1,3 @@
 export { anonymizeTournamentRecord } from '@Generators/tournamentRecords/anonymizeTournamentRecord';
+export { completeDrawMatchUps } from '@Generators/mocks/completeDrawMatchUps';
 export { modifyTournamentRecord } from '@Generators/mocks/modifyTournamentRecord';
